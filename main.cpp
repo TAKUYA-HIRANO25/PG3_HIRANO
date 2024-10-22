c24165747b1c89351114ce6561c83249dfac24a2
TAKUYA-HIRANO25 <k023g0013@g.neec.ac.jp>
2024-10-22 12:57:27 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -3,47 +3,58 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-
+#include <functional>  
 
 // コールバック関数
-typedef void (*PFunc)(bool);
+typedef void (*PFunc)(int*);
 
-void DisplayResult(bool Answer) {
-    if (Answer) {
-        printf("正解\n");
-    }
-    else {
-        printf("不正解\n");
-    }
+void SetTime(PFunc callback, int time, int result) {
+    Sleep(time);
+    callback(&result);
 }
 
-bool IsEven(int number) {
-    return number % 2 == 0;
+void DisplayResult(int* Answer) {
+    printf("結果は%d\n", *Answer);
 }
 
 int RollDice() {
     return rand() % 6 + 1;
 }
-void GuessOddOrEven(PFunc callback) {
+
+void GuessOddOrEven() {
     int dice = RollDice();
     int userGuess;
-    bool Even = IsEven(dice);
 
-    printf("サイコロの出目が奇数か偶数か当てて!(奇数: 0, 偶数: 1): ");
+
+    printf("サイコロの出目が奇数か偶数か当てて!(奇数: 1, 偶数: 2): ");
     scanf_s("%d", &userGuess);
 
-    bool userIsEven = userGuess;
+    auto judgement = [userGuess, dice]()
+        {
+            int Even = (dice % 2 == 0); 
+
+            if ((userGuess == 1 && !Even) || (userGuess == 2 && Even))
+            {
+                printf("正解です！\n");
+            }
+            else
+            {
+                printf("不正解です...\n");
+            }
+        };
+
 
     printf("判定中...\n");
-    Sleep(3000); 
+    SetTime(DisplayResult, 3000, dice);
 
-    callback(Even == userIsEven);
+    judgement();
+
 }
 
 int main() {
     srand(time(NULL));
 
-    GuessOddOrEven(DisplayResult);
+    GuessOddOrEven();
 
     return 0;
 }
